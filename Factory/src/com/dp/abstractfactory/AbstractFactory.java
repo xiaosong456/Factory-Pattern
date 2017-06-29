ac2a560e73b3ea4d9b78e449c5722c2cdf1a8c68
lxs <1643449009@qq.com>
2017-06-29 09:02:25 +0800

    This is also a test

--- Factory/src/com/dp/abstractfactory/AbstractFactory.java
+++ Factory/src/com/dp/abstractfactory/AbstractFactory.java
@@ -1,7 +1,7 @@
 package com.dp.abstractfactory;
 
 public abstract class AbstractFactory {
-	/*创建所有的功能的抽象方法*/
+	/*创建所有的功能的抽象方法  */
 	public abstract Vehicle createVehicle();
 	public abstract Weapon createWeapon();
 }
